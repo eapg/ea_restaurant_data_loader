1b1bbfaffc51808c4f9c76f478fc15b4a74912fb
eapg <elidopg@gmail.com>
2023-10-11 20:24:55 -0400

    adding product model and migration

---
+++ ea_restaurant_data_loader/priv/repo/migrations/20231013004352_create_products.exs
@@ -0,0 +1,17 @@
+defmodule EaRestaurantDataLoader.Repo.Migrations.CreateProducts do
+  use Ecto.Migration
+
+  def change do
+    create table(:products) do
+      add :name, :string
+      add :description, :string
+      add :entity_status, :string
+      add :created_by, references(:users)
+      add :updated_by, references(:users)
+      add :created_date, :naive_datetime
+      add :updated_date, :naive_datetime
+
+      timestamps()
+    end
+  end
+end
